19029e900de565b65097058e2d1b46ba727027a5
Aleksander Kruk <kruk.aleksander@hotmail.com>
2020-10-15 09:04:02 +0200

    second method create, remove description

--- bmr.ts
+++ bmr.ts
@@ -1,31 +1,44 @@
-type Gender = 'woman' | 'man';
+type Gender = "woman" | "man";
 
 interface BMR {
-    bodyMass: number,
-    height: number,
-    age: number,
-    gender: Gender
-};
+  bodyMass: number;
+  height: number;
+  age: number;
+  gender: Gender;
+}
 
 class Bmr {
-    bodyMass: number;
-    height: number;
-    age: number;
-    gender: Gender;
-    description: string = 'BMR (z ang. basal metabolic rate), czyli wskaźnik podstawowej przemiany materii (polski skrót PPM), nazywany również tempem metabolizmu podstawowego, wskazuje najmniejsze dzienne zapotrzebowanie kaloryczne, dzięki któremu zachowamy podstawowe funkcje życiowe. Na układ nerwowy przypada 1/4 tej wartości, na wątrobę - 1/5, 1/15 na serce i nerki, a kolejne 2/5 na pozostałe organy.'
+  bodyMass: number;
+  height: number;
+  age: number;
+  gender: Gender;
 
-constructor(bodyMass: number, height: number, age: number, gender: Gender) {
+  constructor(bodyMass: number, height: number, age: number, gender: Gender) {
     this.bodyMass = bodyMass;
     this.height = height;
-    this.age = age;    
-}
+    this.age = age;
+  }
 
-harrisBenedictMethod() {
-    if(this.gender === 'man') {
-        return 66 + (13.7 * this.bodyMass) + (5 * this.height) - (6.76 * this.age)
+  harrisBenedictMethod() {
+    if (this.gender === "man") {
+      return 66 + 13.7 * this.bodyMass + 5 * this.height - 6.76 * this.age;
     } else {
-        return 655 + (9.6 * this.bodyMass) + (1.8 * this.height) - (4.7 * this.age)
+      return 655 + 9.6 * this.bodyMass + 1.8 * this.height - 4.7 * this.age;
     }
-    
-}
+  }
+  mifflinStJeorMethod() {
+    const manVar: number = 5;
+    const womanVar: number = -161;
+    const formula = (variable: number) => {
+      return (
+        9.99 * this.bodyMass + 6.25 * this.height - 4.92 * this.age + variable
+      );
+    };
+
+    if (this.gender === "man") {
+      return formula(manVar);
+    } else {
+      return formula(womanVar);
+    }
+  }
 }
